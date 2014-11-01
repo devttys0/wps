86615fada59fa45782b3bba1822af51b0b69ee33
devttys0 <heffnercj@gmail.com>
2014-10-31 22:12:40 -0400

    Fixed erroneous comments.

--- pingens/dlink/pingen.py
+++ pingens/dlink/pingen.py
@@ -78,16 +78,14 @@ class DLink(object):
                      ((pin & 0x0F) << 20))
 
         # The largest possible remainder for any value divided by 10,000,000
-        # is 9,999,999 (7 digits). Since nic is a 3-byte value, the largest
-        # possible value for nic is 16,777,215; thus the largest possible remainder
-        # here is 6,777,215. The smallest possible remainder is, obviously, 0.
+        # is 9,999,999 (7 digits). The smallest possible remainder is, obviously, 0.
         pin = pin % int(10e6)
 
-        # If the nic is less than 1,000,000 (i.e., less than 7 digits)
+        # If the pin is less than 1,000,000 (i.e., less than 7 digits)
         if pin < int(10e5):
             # The largest possible remainder for any value divided by 9 is
-            # 8; hence this adds at most 9,000,000 to the nic value, and at
-            # least 1,000,000. This guarantees that the nic will be 7 digits
+            # 8; hence this adds at most 9,000,000 to the pin value, and at
+            # least 1,000,000. This guarantees that the pin will be 7 digits
             # long, and also means that it won't start with a 0.
             pin += ((pin % 9) * int(10e5)) + int(10e5);
 
